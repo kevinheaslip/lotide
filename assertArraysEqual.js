9e81f137ef1a794621905924fac724b63f9a41a5
Kevin Heaslip <kevin.heaslip@mailbox.org>
2023-02-26 10:07:04 -0800

    Fixed the last remaining bug in eqArrays

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -2,17 +2,16 @@
 const eqArrays = function(arr1, arr2) {
   if (arr1.length === arr2.length) {
     // checks if both arrays are empty, and if so, returns true
-    if (arr1.length === 0) {
+    if (!arr1.length) {
       return true;
     } else {
       // checks to see if the elements in both arrays are the same
       for (let i = 0; i < arr1.length; i++) {
         if (arr1[i] !== arr2[i]) {
           return false;
-        } else {
-          return true;
         }
       }
+      return true;
     }
   }
 };
