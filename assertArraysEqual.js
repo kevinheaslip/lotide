5f876995442863d3a0dbd07e5086eadc7a51c59f
Kevin Heaslip <kevin.heaslip@mailbox.org>
2023-02-25 17:31:07 -0800

    Added assertArraysEqual function

---
+++ assertArraysEqual.js
@@ -0,0 +1,26 @@
+// a function that compares if two input arrays are equal
+const eqArrays = function(arr1, arr2) {
+  let eqElements = false;
+  if (arr1.length === arr2.length) {
+    for (let i = 0; i < arr1.length; i++) {
+      if (arr1[i] === arr2[i]) {
+        eqElements = true;
+      } else {
+        eqElements = false;
+      }
+    }
+  }
+  return eqElements;
+};
+
+// a function that logs to the console whether or not two arrays are equal
+const assertArraysEqual = function(arr1, arr2) {
+  if (eqArrays(arr1, arr2)) {
+    console.log(`🟢🟢🟢 Assertion Passed: ${arr1} === ${arr2}`);
+  } else {
+    console.log(`🔴🔴🔴 Assertion Failed: ${arr1} !== ${arr2}`);
+  }
+};
+
+assertArraysEqual([1, 2, 3], [1, 2, 3]);
+assertArraysEqual([1, 2, 3], [3, 2, 1]);
