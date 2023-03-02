2f2fca186bec079950d86189a752610e106e5d1c
Kevin Heaslip <kevin.heaslip@mailbox.org>
2023-03-01 19:04:38 -0800

    Added countLetters function

---
+++ countLetters.js
@@ -0,0 +1,40 @@
+// checks if two inputs are equivalent
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`🟢🟢🟢 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🔴🔴🔴 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+// accepts a sentence as a string and returns a count of each of the letters in the string
+const countLetters = function(sentence) {
+  const results = {};
+  for (letter of sentence) {
+    if (letter !== " ") {
+      if (results[letter]) {
+        results[letter] += 1;
+      } else {
+        results[letter] = 1;
+      }
+    }
+  }
+  return results;
+};
+
+// test cases
+console.log(countLetters("lighthouse in the house"))
+
+// expected output
+/* {
+  l:1,
+  i: 2,
+  g: 1,
+  h: 4,
+  t: 2,
+  o: 2,
+  u: 2,
+  s: 2,
+  e: 3,
+  n: 1
+} */
