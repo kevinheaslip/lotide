63e973a7a73d90f758a3645cf7fb402b3639115f
Kevin Heaslip <kevin.heaslip@mailbox.org>
2023-03-04 15:05:08 -0800

    Added map function

---
+++ map.js
@@ -0,0 +1,42 @@
+// a function that checks to see if two arrays are equal
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length === arr2.length) {
+    // checks if both arrays are empty, and if so, returns true
+    if (!arr1.length) {
+      return true;
+    } else {
+      // checks to see if the elements in both arrays are the same
+      for (let i = 0; i < arr1.length; i++) {
+        if (arr1[i] !== arr2[i]) {
+          return false;
+        }
+      }
+      return true;
+    }
+  }
+};
+
+// a function that logs to the console whether or not two arrays are equal
+const assertArraysEqual = function(arr1, arr2) {
+  if (eqArrays(arr1, arr2)) {
+    console.log(`🟢🟢🟢 Assertion Passed: ${arr1} === ${arr2}`);
+  } else {
+    console.log(`🔴🔴🔴 Assertion Failed: ${arr1} !== ${arr2}`);
+  }
+};
+
+// accepts an array and callback function and returns a new array based on the results of the callback function
+const words = ["ground", "control", "to", "major", "tom"];
+
+const map = function(array, callback) {
+  const results = [];
+  for (let item of array) {
+    results.push(callback(item));
+  }
+  return results;
+};
+
+// test cases
+assertArraysEqual(map(words, word => word.length), [6, 7, 2, 5, 3]);
+assertArraysEqual(map(words, word => word.toUpperCase()), ["GROUND", "CONTROL", "TO", "MAJOR", "TOM"]);
+assertArraysEqual(map(words, word => word.slice(2)), ['ound', 'ntrol', '', 'jor', 'm']);
