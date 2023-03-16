d8c138010a4fb216943909e9943b4cc346970231
Kevin Heaslip <kevin.heaslip@mailbox.org>
2023-03-15 22:08:29 -0700

    Refactored to move tests to the test directory and export assertEqual

--- assertEqual.js
+++ assertEqual.js
@@ -7,10 +7,4 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-// test cases
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-assertEqual("cat", "cat");
-assertEqual("cat", "dog");
-assertEqual(42, 42);
-assertEqual(42, 3);
+module.exports = assertEqual;
