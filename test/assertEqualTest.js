ba30c7f8d73c0c6114c7e4096ce4ffa5f38a18bd
Kevin Heaslip <kevin.heaslip@mailbox.org>
2023-03-15 22:09:47 -0700

    Added test directory, assertEqualTest and headTest

---
+++ test/assertEqualTest.js
@@ -0,0 +1,9 @@
+const assertEqual = require('../assertEqual');
+
+// test cases
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual("cat", "cat");
+assertEqual("cat", "dog");
+assertEqual(42, 42);
+assertEqual(42, 3);
