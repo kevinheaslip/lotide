126a234cb88e304a7ff31264235b7b60bda28a0e
Kevin Heaslip <kevin.heaslip@mailbox.org>
2023-03-17 16:51:08 -0700

    Refactor and move test code to assertArraysEqualTest

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,20 +1,4 @@
-// a function that checks to see if two arrays are equal
-const eqArrays = function(arr1, arr2) {
-  if (arr1.length === arr2.length) {
-    // checks if both arrays are empty, and if so, returns true
-    if (!arr1.length) {
-      return true;
-    } else {
-      // checks to see if the elements in both arrays are the same
-      for (let i = 0; i < arr1.length; i++) {
-        if (arr1[i] !== arr2[i]) {
-          return false;
-        }
-      }
-      return true;
-    }
-  }
-};
+const eqArrays = require('./eqArrays');
 
 // a function that logs to the console whether or not two arrays are equal
 const assertArraysEqual = function(arr1, arr2) {
@@ -25,5 +9,4 @@ const assertArraysEqual = function(arr1, arr2) {
   }
 };
 
-assertArraysEqual([1, 2, 3], [1, 2, 3]);
-assertArraysEqual([1, 2, 3], [3, 2, 1]);
+module.exports = assertArraysEqual;
