bb6aaf1c51e092f612849d6715b90e125987a0db
Kevin Heaslip <kevin.heaslip@mailbox.org>
2023-03-17 17:48:41 -0700

    Refactor test code to use mocha and chai

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,15 +1,14 @@
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
 const tail = require('../tail');
 
-// test case: check if the original array is unmodified
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words);
-assertEqual(words.length, 3);
-
-// test case: an array with one element should yield an empty array for its tail
-const animals = ["cat"];
-assertEqual(tail(animals).length, 0);
-
-// test case: an empty array should yield an empty array for its tail
-const emptyArray = [];
-assertEqual(tail(emptyArray).length, 0);
+describe("#tail", () => {
+  it("returns ['Lighthouse', 'Labs'] for ['Yo Yo', 'Lighthouse', 'Labs']", () => {
+    assert.deepEqual(tail(['Yo Yo', 'Lighthouse', 'Labs']), ['Lighthouse', 'Labs']);
+  });
+  it("returns [] for ['cat']", () => {
+    assert.deepEqual(tail(['cat']), []);
+  });
+  it("returns [] for []", () => {
+    assert.deepEqual(tail([]), []);
+  });
+});
