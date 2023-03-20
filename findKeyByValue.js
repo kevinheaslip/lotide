62ac9db8412536c484f64423cfcb2b1910c26a56
Kevin Heaslip <kevin.heaslip@mailbox.org>
2023-03-19 23:58:16 -0700

    Refactor and move test code to findKeyByValueTest

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -1,12 +1,3 @@
-// checks if two inputs are equivalent
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`🟢🟢🟢 Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🔴🔴🔴 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 // a function that searches an object for a value and returns the first key for that value
 const findKeyByValue = function(object, value) {
   const keys = Object.keys(object);
@@ -18,14 +9,4 @@ const findKeyByValue = function(object, value) {
   return undefined;
 };
 
-// test cases
-const bestTVShowsByGenre = {
-  sci_fi: "The Expanse",
-  comedy: "Brooklyn Nine-Nine",
-  drama: "The Wire"
-};
-
-assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
-assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
-assertEqual(findKeyByValue(bestTVShowsByGenre, "The Expanse"), "sci_fi");
-assertEqual(findKeyByValue(bestTVShowsByGenre, "Alf"), undefined);
+module.exports = findKeyByValue;
