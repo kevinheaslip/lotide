d06a248cc2a46246a3716b1797229a4e78312908
Kevin Heaslip <kevin.heaslip@mailbox.org>
2023-03-19 23:10:11 -0700

    Refactor flatten and move test code to FlattenTest

--- flatten.js
+++ flatten.js
@@ -1,30 +1,3 @@
-// a function that checks to see if two arrays are equal
-const eqArrays = function(arr1, arr2) {
-  if (arr1.length === arr2.length) {
-    // checks if both arrays are empty, and if so, returns true
-    if (!arr1.length) {
-      return true;
-    } else {
-      // checks to see if the elements in both arrays are the same
-      for (let i = 0; i < arr1.length; i++) {
-        if (arr1[i] !== arr2[i]) {
-          return false;
-        }
-      }
-      return true;
-    }
-  }
-};
-
-// a function that logs to the console whether or not two arrays are equal
-const assertArraysEqual = function(arr1, arr2) {
-  if (eqArrays(arr1, arr2)) {
-    console.log(`🟢🟢🟢 Assertion Passed: ${arr1} === ${arr2}`);
-  } else {
-    console.log(`🔴🔴🔴 Assertion Failed: ${arr1} !== ${arr2}`);
-  }
-};
-
 // a function that accepts an array containing a nested array of elements and returns a flattened array
 const flatten = function(arr) {
   const flatArr = [];
@@ -40,7 +13,4 @@ const flatten = function(arr) {
   return flatArr;
 };
 
-// test cases
-assertArraysEqual(flatten([1, 2, [3, 4], 5, [6]]), [1, 2, 3, 4, 5, 6]);
-assertArraysEqual(flatten([15, 1, [], [42], 12]), [15, 1, 42, 12]);
-assertArraysEqual(flatten([1, 2, 3, 4, 5]), [1, 2, 3, 4, 5]);
+module.exports = flatten;
