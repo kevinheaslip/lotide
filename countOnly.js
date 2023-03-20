8d3a9f26530998b29b66e9f7e96c921b3a89a20a
Kevin Heaslip <kevin.heaslip@mailbox.org>
2023-03-19 23:31:19 -0700

    Refactor and move test code to countOnlyTest

--- countOnly.js
+++ countOnly.js
@@ -1,12 +1,3 @@
-// checks if two inputs are equivalent
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`🟢🟢🟢 Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🔴🔴🔴 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 // takes in a collection of items and returns counts for a specific subset of those items
 // allItems: an array of strings that we need to look through
 // itemsToCount: an object specifying what to count
@@ -24,22 +15,4 @@ const countOnly = function(allItems, itemsToCount) {
   return results;
 };
 
-// test cases
-const firstNames = [
-  "Karl",
-  "Salima",
-  "Agouhanna",
-  "Fang",
-  "Kavith",
-  "Jason",
-  "Salima",
-  "Fang",
-  "Joe"
-];
-
-const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
-
-assertEqual(result1["Jason"], 1);
-assertEqual(result1["Karima"], undefined);
-assertEqual(result1["Fang"], 2);
-assertEqual(result1["Agouhanna"], undefined);
+module.exports = countOnly;
