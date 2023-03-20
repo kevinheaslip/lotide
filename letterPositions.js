02b3c994777e7b65d187b41c738b8c58ac845092
Kevin Heaslip <kevin.heaslip@mailbox.org>
2023-03-19 23:27:13 -0700

    Refactor and move test code to letterPositionsTest

--- letterPositions.js
+++ letterPositions.js
@@ -1,30 +1,3 @@
-// a function that checks to see if two arrays are equal
-const eqArrays = function(arr1, arr2) {
-  if (arr1.length === arr2.length) {
-    // checks if both arrays are empty, and if so, returns true
-    if (!arr1.length) {
-      return true;
-    } else {
-      // checks to see if the elements in both arrays are the same
-      for (let i = 0; i < arr1.length; i++) {
-        if (arr1[i] !== arr2[i]) {
-          return false;
-        }
-      }
-      return true;
-    }
-  }
-};
-
-// a function that logs to the console whether or not two arrays are equal
-const assertArraysEqual = function(arr1, arr2) {
-  if (eqArrays(arr1, arr2)) {
-    console.log(`🟢🟢🟢 Assertion Passed: ${arr1} === ${arr2}`);
-  } else {
-    console.log(`🔴🔴🔴 Assertion Failed: ${arr1} !== ${arr2}`);
-  }
-};
-
 // returns all indices in a string where a letter can be found
 const letterPositions = function(sentence) {
   const results = {};
@@ -33,14 +6,10 @@ const letterPositions = function(sentence) {
       if (!results[sentence[i]]) {
         results[sentence[i]] = [];
       }
-      results[sentence[i]].push(i); 
+      results[sentence[i]].push(i);
     }
   }
   return results;
 };
 
-// test cases
-assertArraysEqual(letterPositions("hello").h, [0]);
-assertArraysEqual(letterPositions("hello").e, [1]);
-assertArraysEqual(letterPositions("hello").l, [2, 3]);
-assertArraysEqual(letterPositions("hello").o, [4]);
+module.exports = letterPositions;
