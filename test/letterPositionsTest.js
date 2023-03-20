e0eb34ef2d24a483c9b11b1636c1725dfd0647ec
Kevin Heaslip <kevin.heaslip@mailbox.org>
2023-03-19 23:27:45 -0700

    Add test code and refactor to use mocha and chai

---
+++ test/letterPositionsTest.js
@@ -0,0 +1,17 @@
+const assert = require('chai').assert;
+const letterPositions = require('../letterPositions');
+
+describe('#letterPositions', () => {
+  it("returns [0] for letterPositions('hello').h", () => {
+    assert.deepEqual(letterPositions("hello").h, [0]);
+  });
+  it("returns [1] for letterPositions('hello').e", () => {
+    assert.deepEqual(letterPositions("hello").e, [1]);
+  });
+  it("returns [2, 3] for letterPositions('hello').l", () => {
+    assert.deepEqual(letterPositions("hello").l, [2, 3]);
+  });
+  it("returns [4] for letterPositions('hello').0", () => {
+    assert.deepEqual(letterPositions("hello").o, [4]);
+  });
+});
